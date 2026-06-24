d8d75c4823a30c5dfab3ad32eb87c1b986e1454b
Jayendra Patel <pjayendra2005@gmail.com>
2026-06-24 10:05:48 +0530

    Time: 2109 ms (14.41%), Space: 669.3 MB (6.31%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
@@ -1,44 +1,39 @@
 #define ll long long
+const int mod = 1e9 + 7;
 class Solution {
 public:
-    const int mod = 1e9 + 7;
     int zigZagArrays(int n, int l, int r) {
-        ll m = r - l + 1;
+        int m = r - l + 1;
 
-        if (n == 1) {
-            return m;
+        vector<vector<ll>> prev_vec(m + 1, vector<ll>(2, 0));
+        vector<vector<ll>> curr_vec(m + 1, vector<ll>(2, 0));
+
+        // base case
+        for (int prev = 1; prev <= m; prev++) {
+            prev_vec[prev][1] = 1;
+            prev_vec[prev][0] = 1;
         }
 
-        vector<ll> up(m + 1);
-        vector<ll> down(m + 1);
+        for (int i = n - 1; i >= 1; i--) {
 
-        for (int v = 1; v <= m; v++) {
-            up[v] = v - 1;
-            down[v] = m - v;
-        }
+            vector<ll> cumsum0(m + 1);
+            vector<ll> cumsum1(m + 1);
 
-        for (int i = 2; i < n; i++) {
-            vector<ll> newup(m + 1);
-            vector<ll> newdown(m + 1);
-
-            ll pref = 0, suff = 0;
-            for (int v = 1; v <= m; v++) {
-                newup[v] = pref;
-                pref = (pref + down[v]) % mod;
+            for (int val = 1; val <= m; val++) {
+                cumsum0[val] = (cumsum0[val - 1] + prev_vec[val][0]) % mod;
+                cumsum1[val] = (cumsum1[val - 1] + prev_vec[val][1]) % mod;
             }
 
-            for (int v = m; v >= 1; v--) {
-                newdown[v] = suff;
-                suff = (suff + up[v]) % mod;
+            for (int prev = 1; prev <= m; prev++) {
+                curr_vec[prev][0] = cumsum1[prev - 1];
+                curr_vec[prev][1] = (cumsum0[m] - cumsum0[prev] + mod) % mod;
             }
-
-            up = newup;
-            down = newdown;
+            prev_vec = curr_vec;
         }
 
         ll ans = 0;
-        for (int v = 1; v <= m; v++) {
-            ans = (ans + up[v] + down[v]) % mod;
+        for (int prev = 1; prev <= m; prev++) {
+            ans = (ans + prev_vec[prev][0] + prev_vec[prev][1]) % mod;
         }
 
         return ans;
