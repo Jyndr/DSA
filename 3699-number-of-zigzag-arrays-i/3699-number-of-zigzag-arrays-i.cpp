e5f1993c89ef3dfaef870e1ffde243dea50a65f1
Jayendra Patel <pjayendra2005@gmail.com>
2026-06-24 10:07:05 +0530

    Time: 1041 ms (35.14%), Space: 720.2 MB (5.41%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
@@ -5,13 +5,13 @@ public:
     int zigZagArrays(int n, int l, int r) {
         int m = r - l + 1;
 
-        vector<vector<ll>> prev_vec(m + 1, vector<ll>(2, 0));
-        vector<vector<ll>> curr_vec(m + 1, vector<ll>(2, 0));
+        ll dp[n + 1][m + 1][2];
+        memset(dp, 0, sizeof(dp));
 
         // base case
         for (int prev = 1; prev <= m; prev++) {
-            prev_vec[prev][1] = 1;
-            prev_vec[prev][0] = 1;
+            dp[n][prev][1] = 1;
+            dp[n][prev][0] = 1;
         }
 
         for (int i = n - 1; i >= 1; i--) {
@@ -20,20 +20,19 @@ public:
             vector<ll> cumsum1(m + 1);
 
             for (int val = 1; val <= m; val++) {
-                cumsum0[val] = (cumsum0[val - 1] + prev_vec[val][0]) % mod;
-                cumsum1[val] = (cumsum1[val - 1] + prev_vec[val][1]) % mod;
+                cumsum0[val] = ( cumsum0[val - 1] + dp[i + 1][val][0] ) % mod;
+                cumsum1[val] = ( cumsum1[val - 1] + dp[i + 1][val][1] ) % mod;
             }
 
             for (int prev = 1; prev <= m; prev++) {
-                curr_vec[prev][0] = cumsum1[prev - 1];
-                curr_vec[prev][1] = (cumsum0[m] - cumsum0[prev] + mod) % mod;
+                dp[i][prev][0] = cumsum1[prev - 1];
+                dp[i][prev][1] = (cumsum0[m] - cumsum0[prev] + mod) % mod;
             }
-            prev_vec = curr_vec;
         }
 
         ll ans = 0;
         for (int prev = 1; prev <= m; prev++) {
-            ans = (ans + prev_vec[prev][0] + prev_vec[prev][1]) % mod;
+            ans = (ans + dp[1][prev][0] + dp[1][prev][1]) % mod;
         }
 
         return ans;
