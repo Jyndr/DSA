8f42e7f7053251464ed8b2db8ab95829190ee59e
Jayendra Patel <pjayendra2005@gmail.com>
2026-07-06 18:03:11 +0530

    Time: 1 ms (27.83%), Space: 9.2 MB (67.95%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,48 +1,27 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        
-
-        // create hashMap
+        int cnt = 0;
 
         unordered_map<char, int> mpp;
-        //"abccccdd"
 
-        for (char ch : s) {
-            mpp[ch]++;
+        for (int i = 0; i < s.size(); i++) {
+            mpp[s[i]]++;
         }
 
-       
+        bool one_odd = false;
 
-        // a: 1, b: 1, c: 4, d: 2
-        bool selectFreqOne = false;
-        int count = 0;
-        for (char ch: s) {
-            
-            // pick freq is one
-            if (!selectFreqOne && mpp[ch] == 1) {
-                count += 1;
-                selectFreqOne = true;
-                mpp[ch] = 0;
-            }
-            // pick freq is even
-            if (mpp[ch] != 0 && mpp[ch] % 2 == 0) {
-                count += mpp[ch];
-                mpp[ch] = 0;
-                
-            }
-
-            // pick freq is odd
-            if (mpp[ch] != 1 && mpp[ch] % 2 != 0) {
-                // we have to add even freq and make count 1
-                int evenFreq = mpp[ch] - 1;
-                count += evenFreq;
-                // we have to make count 1
-
-                mpp[ch] = 1;
+        for (auto it : mpp) {
+            if (it.second % 2 == 1 && !one_odd) {
+                cnt += it.second;
+                one_odd = !one_odd;
+            } else if (it.second % 2 == 1) {
+                cnt += it.second - 1;
+            } else {
+                cnt += it.second;
             }
         }
 
-        return count;
+        return cnt;
     }
 };
