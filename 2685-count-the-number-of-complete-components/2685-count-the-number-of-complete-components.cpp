ec7e041cbd0082e811a830896c33f899ba3dd2d0
Jayendra Patel <pjayendra2005@gmail.com>
2026-07-12 11:20:06 +0530

    Time: 12 ms (94.57%), Space: 117.7 MB (95.28%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,45 +1,77 @@
+class DSU {
+public:
+    vector<int> parent;
+    vector<int> sizee;
+
+    DSU(int n) {
+        parent.resize(n);
+        sizee.resize(n, 1);
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int ultimate_p(int u) {
+        if (parent[u] == u) {
+            return u;
+        }
+        return parent[u] = ultimate_p(parent[u]);
+    }
+
+    void union_by_size(int u, int v) {
+        int ulp_u = ultimate_p(u);
+        int ulp_v = ultimate_p(v);
+
+        if (ulp_u == ulp_v) {
+            return;
+        }
+
+        if (sizee[ulp_u] > sizee[ulp_v]) {
+            sizee[ulp_u] += sizee[ulp_v];
+            parent[ulp_v] = ulp_u;
+        } else {
+            sizee[ulp_v] += sizee[ulp_u];
+            parent[ulp_u] = ulp_v;
+        }
+    }
+};
 class Solution {
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        // adjacency lists for each vertex
-        vector<vector<int>> graph(n);
-        // map to store frequency of each unique adjacency list
-        unordered_map<string, int> componentFreq;
 
-        // initialize adjacency lists with self-loops
-        for (int vertex = 0; vertex < n; vertex++) {
-            graph[vertex].push_back(vertex);
+        DSU tree(n);
+
+        vector<int> edge(n);
+        vector<int> nodes_in_component(n);
+
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            tree.union_by_size(u, v);
+            edge[u]++;
+            edge[v]++;
         }
 
-        // build adjacency lists from edges
-        for (const auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
+        for (int i = 0; i < n; i++) {
+            nodes_in_component[tree.parent[i]]++;
         }
 
-        // count frequency of each unique adjacency pattern
-        for (int vertex = 0; vertex < n; vertex++) {
-            vector<int> neighbors = graph[vertex];
-            sort(neighbors.begin(), neighbors.end());
+        vector<bool> is_completed(n, true);
 
-            // convert vector to string for hashing
-            string key;
-            for (int num : neighbors) {
-                key += to_string(num) + ",";
-            }
-            componentFreq[key]++;
+        for (int i = 0; i < n; i++) {
+            int p = tree.parent[i];
+            int req = nodes_in_component[p] - 1;
+            is_completed[p] = is_completed[p] && (edge[i] == req);
         }
 
-        // count complete components where size equals frequency
-        int completeCount = 0;
-        for (const auto& entry : componentFreq) {
-            // count commas to get original vector size
-            int size = count(entry.first.begin(), entry.first.end(), ',');
-            if (size == entry.second) {
-                completeCount++;
-            }
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            cnt += (tree.parent[i] == i && is_completed[i]);
         }
 
-        return completeCount;
+        return cnt;
     }
 };
