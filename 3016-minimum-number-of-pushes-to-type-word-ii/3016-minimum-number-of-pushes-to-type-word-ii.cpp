4a66d0ee1df09727f9a8c918dab2fbb274928f9d
Jayendra Patel <pjayendra2005@gmail.com>
2026-08-22 22:28:23 +0530

    Time: 19 ms (22.39%), Space: 28.6 MB (5.45%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        // Frequency vector to store count of each letter
-        vector<int> frequency(26, 0);
 
-        // Count occurrences of each letter
-        for (char& c : word) {
-            ++frequency[c - 'a'];
+        vector<int> v(26);
+
+        for (int i = 0; i < word.size(); i++) {
+            v[word[i] - 'a']++;
         }
 
-        // Sort frequencies in descending order
-        sort(frequency.rbegin(), frequency.rend());
-
-        int totalPushes = 0;
-
-        // Calculate total number of presses
-        for (int i = 0; i < 26; ++i) {
-            if (frequency[i] == 0) break;
-            totalPushes += (i / 8 + 1) * frequency[i];
+        vector<int> sorted;
+        for (int i = 0; i < 26; i++) {
+            if (v[i]) {
+                sorted.push_back(v[i]);
+            }
         }
 
-        return totalPushes;
+        sort(sorted.rbegin() , sorted.rend());
+
+        int ans = 0;
+
+        for(int i = 0;i<sorted.size();i++){
+            ans += sorted[i] * (i / 8 + 1);
+        }
+
+        return ans;
     }
 };
