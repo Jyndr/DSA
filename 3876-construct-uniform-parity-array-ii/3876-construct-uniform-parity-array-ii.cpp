987988ba734de266704481e4ddbc0f6fc7b65f5a
Jayendra Patel <pjayendra2005@gmail.com>
2026-09-03 10:14:22 +0530

    Time: 0 ms (100%), Space: 165.8 MB (62.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         bool all_odd = true, all_even = true;
-        int min_even = INT_MAX;
+        int min_ele = INT_MAX;
 
         for (int i = 0; i < nums1.size(); i++) {
             if (nums1[i] % 2 == 0) {
                 all_odd = false;
-                min_even = min(min_even, nums1[i]);
             } else {
                 all_even = false;
             }
+            min_ele = min(min_ele, nums1[i]);
         }
-        if (all_odd || all_even) {
+        if (all_odd || all_even || min_ele % 2 == 1) {
             return true;
-        }
-
-        for (int i = 0; i < nums1.size(); i++) {
-            if (nums1[i] % 2 == 1 && nums1[i] < min_even) {
-                return true;
-            }
         }
         return false;
     }
